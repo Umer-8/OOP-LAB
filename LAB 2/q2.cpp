3d01eba08276e53639fef9b775c5797c6d1f7483
Umer-8 <muhammadumer4790@gmail.com>
2025-02-04 23:09:24 +0500

    Update q2.cpp
    updated code

--- LAB 2/q2.cpp
+++ LAB 2/q2.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
-void* add (void* array) {
- int *arr=(int*)array;
 
-    int result=0;
-    for(int i=0;i<5;i++){
-    result+=arr[i];
+void* addToEachElement(void* arr, int size, int valueToAdd) {
+    for (int i = 0; i < size; i++) {
+        ((int*)arr)[i] += valueToAdd;
     }
-    void *ptr=(void*)&result;
-    return ptr;
+    return arr;
 }
-int main ()
- {
-    int array[5]={10,20,30,40,50};
-       void *ptr=(void*)array;
-    int result=*(int*)add(ptr);
-    cout<<result<<endl;
+
+int main() {
+    int numElements, incrementValue;
+
+    cout << "Enter the number of elements: ";
+    cin >> numElements;
+
+    int* arr = new int[numElements];
+
+    cout << "Enter the value to add to each element: ";
+    cin >> incrementValue;
+
+    for (int i = 0; i < numElements; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    int* updatedArray = (int*)addToEachElement(arr, numElements, incrementValue);
+
+    cout << "The updated array is:\n";
+    for (int i = 0; i < numElements; i++) {
+        cout << updatedArray[i] << "\n";
+    }
+
+    delete[] arr;
+
     return 0;
 }
